b2cd23907dda65e32ab584dbb10ffd44dd9ac3ab
Mahesh085 <69253765+Mahesh085@users.noreply.github.com>
2024-11-11 23:27:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let stick;
+let stick, hero, platform, nextPlatform, score = 0;
+const gameContainer = document.getElementById("game-container");
 
 function startGame() {
     stick = document.getElementById("stick");
+    hero = document.getElementById("hero");
+    platform = document.getElementById("platform");
+    nextPlatform = document.getElementById("next-platform");
+
+    score = 0;
+    updateScore();
     stick.style.height = "0px";
     growStick();
 }
@@ -9,7 +16,7 @@ function startGame() {
 function growStick() {
     let height = 0;
     const growInterval = setInterval(() => {
-        if (height < 150) { // Maximum stick length
+        if (height < 200) {
             height += 5;
             stick.style.height = `${height}px`;
         } else {
@@ -22,4 +29,54 @@ function growStick() {
 function dropStick() {
     stick.style.transition = "transform 0.5s";
     stick.style.transform = "rotate(90deg)";
+    setTimeout(checkIfHeroCanCross, 500);
+}
+
+function checkIfHeroCanCross() {
+    const stickEnd = stick.getBoundingClientRect().right;
+    const nextPlatformStart = nextPlatform.getBoundingClientRect().left;
+    const nextPlatformEnd = nextPlatform.getBoundingClientRect().right;
+
+    if (stickEnd >= nextPlatformStart && stickEnd <= nextPlatformEnd) {
+        score++;
+        updateScore();
+        moveHero();
+    } else {
+        alert("Game Over! Final Score: " + score);
+        resetGame();
+    }
+}
+
+function moveHero() {
+    hero.style.transition = "left 1s";
+    hero.style.left = `${parseInt(hero.style.left) + 250}px`;
+
+    setTimeout(() => {
+        resetStick();
+        repositionPlatforms();
+    }, 1000);
+}
+
+function resetStick() {
+    stick.style.transition = "none";
+    stick.style.transform = "rotate(0deg)";
+    stick.style.height = "0px";
+}
+
+function repositionPlatforms() {
+    platform.style.left = `${parseInt(platform.style.left) + 250}px`;
+    nextPlatform.style.left = `${Math.random() * 500 + 300}px`;
+    startGame();
+}
+
+function resetGame() {
+    hero.style.left = "50px";
+    platform.style.left = "50px";
+    nextPlatform.style.left = "300px";
+    score = 0;
+    updateScore();
+}
+
+function updateScore() {
+    document.getElementById("score").innerText = `Score: ${score}`;
 }
